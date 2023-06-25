a5aba8d647ae34611e4346a68eb0510da77c2d39
Heorhii Honcharov <0019@ukr.net>
2023-06-25 11:09:28 +0200

    modified:   src/components/Reviews/Reviews.jsx

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { ReviewBC, ReviewList, ReviewText } from 'styling/MainContainerCSS';
+import { ReviewBC, ReviewList } from 'styling/MainContainerCSS';
 import { fetchGetImgs } from 'utils/FetchEngine';
 
 const Reviews = () => {
